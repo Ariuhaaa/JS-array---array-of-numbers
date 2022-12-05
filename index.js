075fdb66d3d1f8cbf9ee922980d97b6e63309008
Ariuhaaa <115851178+Ariuhaaa@users.noreply.github.com>
2022-12-05 23:46:59 +0800

    dutuu

---
+++ index.js
@@ -0,0 +1,51 @@
+// 1. arrayOfNumbers гэсэн variable зарлаад дараах тоонуудыг оруул. 43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11
+// 2. Array доторх тоонуудын нийлбэрийг ол.
+// 3. Хамгийн их тоог ол.
+// 4. Хамгийн бага тоог ол.
+// 5. Array - ийн эхэнд дурын 1 тоог нэм.
+// 6. Array - ийн төгсгөлд дурын 1 тоог нэм.
+
+
+//1
+let arrayOfNumbers = [43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11];
+
+
+//2
+let sum = 0;
+
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers [i];
+}
+
+console.log(sum);
+
+//3
+
+//4
+
+//5
+
+arrayOfNumber = [43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11];
+
+// arrayOfNumber.length = 11;
+
+arrayOfNumber[0] = 250;
+
+console.log(arrayOfNumber);
+
+//6
+
+arrayNumber = [43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11];
+
+arrayNumber[11] = 247;
+
+console.log(arrayNumber);
+
+
+
+
+
+
+
+
+
